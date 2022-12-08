42f1fb479128378dfbeb59f5fce9007c8ce15083
Guillermo DAbenigno <gdabenig@hotmail.com>
2022-12-08 15:21:01 -0300

    balance de carga

---
+++ desafio13-balance/routersweb/randoms.js
@@ -0,0 +1,10 @@
+require('child_process')
+randomsWebRouter.get('/api/randoms', (req,res)=>{
+    const cant = parseInt(req.query.cant) || 100000000
+    const forked = fork('calculo.js')
+    forked.send(cant)
+    forked.on('message', msg=>{
+        res.json(msg)
+    })
+})
+module.exports = randomsWebRouter
---
+++ desafio13-balance/server.js
@@ -0,0 +1,93 @@
+onst express = require('express')
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const config = require('./config');
+const hbs = require('express-handlebars');
+const passport = require('passport')
+const {initPassport} = require('./middlewares/passport.js')
+const authWebRouter = require("./routersweb/auth.js");
+const homeWebRouter = require("./routersweb/home.js");
+const infoWebRouter = require("./routersweb/info.js");
+const randomsWebRouter = require("./routersweb/randoms.js")
+
+
+const initServer = () => {
+
+// ---------------------------------------------------------------------------------
+// CONFIGURACION SERVIDOR
+
+    const app = express();
+    app.use(express.json())
+    app.use(express.urlencoded({extended: true}))
+    //app.use(express.static('public'))
+    initPassport()
+
+// ----------------------------------------------------------------------------------
+// MANEJO DE SESSION
+    const advancedOptions = {useNewUrlParser: true, useUnifiedTopology: true}
+
+    app.use(session({
+        store: MongoStore.create({
+            mongoUrl: config.mongoRemote.cnxStr,
+            mongoOptions: advancedOptions
+        }),
+        secret: 'sisisnonono',
+        resave: false,
+        saveUninitialized: false,
+        rolling: true,
+        cookie: {
+            maxAge: 60000
+        }
+    }))
+
+    initPassport()
+    app.use(passport.initialize())
+    app.use(passport.session())
+
+// ----------------------------------------------------------------------------------
+// RUTAS
+
+    app.use(authWebRouter)
+    app.use(homeWebRouter)
+    app.use(infoWebRouter)
+    app.use(randomsWebRouter)
+
+    app.get('/datos', (req, res)=>{
+        console.log(`puerto ${process.port}`)
+        res.send(`Servidor express - PORT ${process.port} - PID : ${process.pid} - FyH: ${new Date().toLocaleString()}`)
+    })
+
+
+
+// ----------------------------------------------------------------------------------
+// SETUP MOTOR PLANTILLAS (HANDLEBARS)
+
+    app.engine(
+        "hbs",
+        hbs.engine({
+            extname: ".hbs",
+            partialDir: "./views/partials",
+            layoutDir: "./views/layouts",
+            defaultLayout: "layout.hbs",
+        })
+    );
+    app.set("views", "./views");
+    app.set("view engine", "hbs");
+
+
+// ----------------------------------------------------------------------------------
+// INICIO DEL SERVER
+
+    return{
+        listen: port => new Promise((res,rej)=>{
+            const server = app.listen(port, () => {
+                console.log(`Servidor escuchando en puerto ${server.address().port}`);
+            });
+            server.on("error", (error) => console.log(`Error en servidor: ${error}`));
+        })
+    }
+
+
+}
+
+module.exports = initServer
